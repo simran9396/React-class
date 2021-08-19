5be64a32603d8b63f57aef9f47bd9eb274b97c60
simran9396 <simranpathan141@gmail.com>
2021-08-19 18:14:47 +0530

    to do list

---
+++ pdac-1/src/To-do-list/AddTodoItem.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { Card, Form, Button, Col, Row } from "react-bootstrap";
+export const AddTodoItem = ({ handleAdd }) => {
+  const [newtodo, setNewtodo] = useState("");
+  return (
+    <Card className="addtodo">
+      Add to do item
+      <Row>
+      <Col>
+        <Form.Control
+          placeholder="add item"
+          onChange={(e) => setNewtodo(e.target.value)}
+        />
+      </Col>
+      <Col>
+        <Button
+          className="BUtton"
+          onClick={() => {
+            handleAdd(newtodo);
+          }}
+          disabled={newtodo === ""}
+        >
+          Add
+        </Button>
+      </Col>
+      </Row>
+    </Card>
+  );
+};
---
+++ pdac-1/src/To-do-list/To-do-list.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import { TodoItem } from './To-do-item'
+
+export const TodoList = ({todoitems}) => {
+    return (
+        <Card className="todolist">
+            <h1>To do list</h1>
+           {todoitems.map((item)=>(<TodoItem item={item}/>))} 
+        </Card>
+    )
+}
